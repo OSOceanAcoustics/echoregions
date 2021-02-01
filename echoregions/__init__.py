eca484e2a84c9b96b73c7b19f8680cf23b860993
Kavin <ng.kavin97@gmail.com>
2021-02-01 01:30:01 -0800

    update with new classes

--- echoregions/__init__.py
+++ echoregions/__init__.py
@@ -1,5 +1,4 @@
 from .convert.ecs_parser import CalibrationParser
-from .convert.ev_parser import EvParserBase
 from .convert.evl_parser import LineParser
 from .convert.evr_parser import Region2DParser
-from .convert import utils
+from .formats.region2d import Region2D
