172dacb2fa1d8a5b372b3b7b4444dc9c557c5bfa
Kavin Nguyen <43619177+ngkavin@users.noreply.github.com>
2021-02-17 00:35:56 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# Dependencies
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+INSTALL_REQUIRES = [t.strip() for t in requirements]
+
+setup(
+    name='echoregions',
+    version='0.1',
+    author='Kavin Nguyen',
+    author_email='ng.kavin97@gmail.com',
+    maintainer='Kavin Nguyen',
+    maintainer_email='ng.kavin97@gmail.com',
+    description='Parsers and functions for working with EVR, ECS, and ECL files',
+    url='https://github.com/OSOceanAcoustics/echoregions',
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(),
+)
