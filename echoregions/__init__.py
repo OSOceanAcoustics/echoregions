2df519cc6f24c1b92d72384e6acbbd00a783df64
Kavin <ng.kavin97@gmail.com>
2021-06-14 14:11:30 -0700

    remove multi regions

--- echoregions/__init__.py
+++ echoregions/__init__.py
@@ -1,5 +1,5 @@
 from .convert.ecs_parser import CalibrationParser
-from .convert.utils import parse_time, parse_filetime, from_JSON
+from .convert.utils import parse_time, parse_filetime
 from .convert.merge import merge
-from .formats import Regions2D, MultiRegions2D, Lines
+from .formats import Regions2D, Lines
 from .convert.read import read_evl, read_evr
--- echoregions/formats/__init__.py
+++ echoregions/formats/__init__.py
@@ -1,3 +1,2 @@
 from .regions2d import Regions2D
-from .multi import MultiRegions2D
 from .lines import Lines
