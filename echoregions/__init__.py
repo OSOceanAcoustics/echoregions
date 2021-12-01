cf85af45365483e7a724b4da0aa3af170130c8b2
leewujung <leewujung@gmail.com>
2021-12-01 16:23:33 -0500

    remove redundant CalibrationParser import

--- echoregions/__init__.py
+++ echoregions/__init__.py
@@ -1,10 +1,8 @@
 from .convert.api import read_ecs, read_evl, read_evr
-from .convert.ecs_parser import CalibrationParser
 from .convert.utils import parse_simrad_fname_time, parse_time
 from .formats import Lines, Regions2D
 
 __all__ = [
-    "CalibrationParser",
     "read_ecs",
     "read_evl",
     "read_evr",
