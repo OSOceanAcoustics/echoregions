594e5ce99463fd98ccdd42bb7b83002d3fbed13f
leewujung <leewujung@gmail.com>
2022-04-14 17:06:20 -0700

    remove read_ecs from init

--- echoregions/__init__.py
+++ echoregions/__init__.py
@@ -3,7 +3,6 @@ from .convert.utils import parse_simrad_fname_time, parse_time
 from .formats import Lines, Regions2D
 
 __all__ = [
-    "read_ecs",
     "read_evl",
     "read_evr",
     "parse_simrad_fname_time",
