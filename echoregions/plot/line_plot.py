0fbd27094a05841fb911d22a5645b89b64e375c6
Caesar Tuguinay <cmtuguinay2000@gmail.com>
2023-05-29 16:23:41 -0700

    add string to datetime conversion
    * added string to datetime conversion since > doesn't operate between Timestamps and strings anymore

--- echoregions/plot/line_plot.py
+++ echoregions/plot/line_plot.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import pandas as pd
 
 class LinesPlotter:
     """Class for plotting Regions. Should only be used by `Lines`"""
@@ -17,11 +17,10 @@ class LinesPlotter:
         **kwargs
     ):
         df = self.Lines.data
-
         if start_time is not None:
-            df = df[df["time"] > start_time]
+            df = df[df["time"] > pd.to_datetime(start_time)]
         if end_time is not None:
-            df = df[df["time"] < end_time]
+            df = df[df["time"] < pd.to_datetime(end_time)]
 
         if fill_between:
             plt.fill_between(df.time, df.depth, max_depth, **kwargs)
