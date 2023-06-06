2f0aea400a5cceb787351863b4caa84f34436150
Caesar Tuguinay <cmtuguinay2000@gmail.com>
2023-06-06 15:50:02 -0700

    add additional ways to select regions
    * used to just be indexing region via string. now you can use few more types to select region to plot

--- echoregions/plot/region_plot.py
+++ echoregions/plot/region_plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-from typing import NewType, Any
+from typing import Union, NewType, Any
+from pandas import Series, DataFrame
 
 # Regions 2D Type Place Holder
 Regions2DType = NewType('Regions2D', Any)
@@ -10,19 +11,24 @@ class Regions2DPlotter:
     def __init__(self, Regions2D: Regions2DType):
         self.Regions2D = Regions2D
 
-    def plot(self, region, close_region=False, **kwargs) -> None:
+    def plot(self, region: Union[float, str, list, Series, DataFrame]=None, 
+             close_region=False, **kwargs) -> None:
         """Plot one or more regions.
 
         Parameters
         ----------
-        region : str
-            DataFrame containing the region(s) to be plotted
+        region : float, str, list, Series, DataFrame, ``None``
+            A region id provided as a number, string, list of these,
+            or a DataFrame/Series containing the region_id column name.
         close_region : bool
             Plot the region as a closed polygon
         kwargs : keyword arguments
             Additional arguments passed to matplotlib plot
         """
+        # TODO Typecheck and add pytest for region
         if close_region:
             region = self.Regions2D.close_region(region)
+        else:
+            region = self.Regions2D.select_region(region)
         for idx, row in region.iterrows():
             plt.plot(row["time"], row["depth"], **kwargs)
