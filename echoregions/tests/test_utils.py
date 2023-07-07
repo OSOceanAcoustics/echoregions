71bda87fe051d1b1866c1b118319094dd6eb74d1
Caesar Tuguinay <cmtuguinay2000@gmail.com>
2023-07-06 17:24:08 -0700

    update test_utils
    * add marks
    * add type hints
    * add short merge test
    * add TODO

--- echoregions/tests/test_utils.py
+++ echoregions/tests/test_utils.py
@@ -1,9 +1,15 @@
 import numpy as np
+import pytest
+from pandas import DataFrame, Series
 
-from echoregions.utils.time import parse_simrad_fname_time, parse_time
+from ..utils.api import merge
+from ..utils.time import parse_simrad_fname_time, parse_time
+
+# TODO Add tests for check_file, from_JSON, validate_path
 
 
-def test_parse_time():
+@pytest.mark.utils
+def test_parse_time() -> None:
     """
     Test converting Echoview datetime string in EVR/EVL to numpy datetime64.
     """
@@ -11,11 +17,27 @@ def test_parse_time():
     assert parse_time(timestamp) == np.datetime64("2017-06-25T15:39:22.3320")
 
 
-def test_parse_filename_time():
+@pytest.mark.utils
+def test_parse_filename_time() -> None:
     """
     Test parsing Simrad-style filename for timestamp.
     """
+
     raw_fname = "Summer2017-D20170625-T124834.raw"
     assert parse_simrad_fname_time(raw_fname) == np.datetime64(
         "2017-06-25T12:48:34.0000"
     )
+
+
+@pytest.mark.utils
+def test_merge_type_checking() -> None:
+    """
+    Test merge type checking functionality.
+    """
+
+    with pytest.raises(ValueError):
+        merge([])
+    with pytest.raises(TypeError):
+        merge([DataFrame()])
+    with pytest.raises(TypeError):
+        merge([Series()])
