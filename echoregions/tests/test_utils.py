9389b783c803d668723071cefa5536b5e0540abf
Caesar Tuguinay <cmtuguinay2000@gmail.com>
2023-07-07 11:09:59 -0700

    add tests for check_file and validate_path
    * add test for check_file errors
    * add test for validate_path errors

--- echoregions/tests/test_utils.py
+++ echoregions/tests/test_utils.py
@@ -1,11 +1,18 @@
+from pathlib import Path
+
 import numpy as np
 import pytest
 from pandas import DataFrame, Series
 
 from ..utils.api import merge
+from ..utils.io import check_file, validate_path
 from ..utils.time import parse_simrad_fname_time, parse_time
 
-# TODO Add tests for check_file, from_JSON, validate_path
+DATA_DIR = Path("./echoregions/test_data/")
+EVR_PATH = DATA_DIR / "transect.evr"
+EVL_PATH = DATA_DIR / "transect.evl"
+EVR_PATH_DNE = DATA_DIR / "transect_DNE.evr"
+EVL_PATH_DNE = DATA_DIR / "transect_DNE.evl"
 
 
 @pytest.mark.utils
@@ -41,3 +48,45 @@ def test_merge_type_checking() -> None:
         merge([DataFrame()])
     with pytest.raises(TypeError):
         merge([Series()])
+
+
+@pytest.mark.utils
+def test_check_file_errors():
+    """
+    Test for check file errors that may arise from improper usage of read_evl and read_evr.
+    """
+
+    # Check invalid mismatch EVR path and EVL format
+    with pytest.raises(ValueError):
+        check_file(EVR_PATH.__str__(), ".EVL")
+    # Check invalid mismatch EVL path and EVR format
+    with pytest.raises(ValueError):
+        check_file(EVL_PATH.__str__(), ".EVR")
+    # Check does not exist EVR path
+    with pytest.raises(ValueError):
+        check_file(EVR_PATH_DNE.__str__(), ".EVR")
+    # Check does not exist EVL path
+    with pytest.raises(ValueError):
+        check_file(EVL_PATH_DNE.__str__(), ".EVL")
+
+
+@pytest.mark.utils
+def test_validate_path_errors():
+    """
+    Test for check file errors that may arise from improper usage of to_csv and to_json.
+    """
+
+    # Check for no path given
+    with pytest.raises(ValueError):
+        validate_path(ext="EVL")
+    # Check for no extension given
+    with pytest.raises(ValueError):
+        validate_path(save_path=EVL_PATH.__str__(), input_file=EVL_PATH_DNE.__str__())
+    # Check for no filename given
+    with pytest.raises(ValueError):
+        validate_path(save_path=EVL_PATH.__str__(), ext="EVL")
+    with pytest.raises(ValueError):
+        # Check for mismatch of file name and file format
+        validate_path(
+            save_path=EVL_PATH.__str__(), input_file=EVL_PATH_DNE.__str__(), ext="EVR"
+        )
