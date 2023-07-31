45dce17acc96231277ad4b6b29446433b8b348bf
Caesar Tuguinay <cmtuguinay2000@gmail.com>
2023-07-31 10:52:08 -0700

    fix SIMRAD utils test
    * change raw_fname from str to list

--- echoregions/tests/test_utils.py
+++ echoregions/tests/test_utils.py
@@ -30,7 +30,7 @@ def test_parse_filename_time() -> None:
     Test parsing Simrad-style filename for timestamp.
     """
 
-    raw_fname = "Summer2017-D20170625-T124834.raw"
+    raw_fname = ["Summer2017-D20170625-T124834.raw"]
     assert parse_simrad_fname_time(raw_fname) == np.datetime64(
         "2017-06-25T12:48:34.0000"
     )
