2ee2ba2a198430c7ba6a07534d8bb0134efa4540
ctuguinay <cmtuguinay2000@gmail.com>
2023-10-02 17:25:39 +0000

    import echoregions_version

--- echoregions/__init__.py
+++ echoregions/__init__.py
@@ -4,6 +4,8 @@ at the beginning of their programming and will produce objects
 that contain the majority of the functionality within echoregions.
 """
 
+from _echoregions_version import version as __version__  # noqa
+
 from .core import read_evl, read_evr
 from .utils.api import convert_mask_2d_to_3d, convert_mask_3d_to_2d
 
