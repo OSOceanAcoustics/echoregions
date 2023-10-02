62ae6bc3fed8ac80bd368764315b3c7225b530b7
ctuguinay <cmtuguinay2000@gmail.com>
2023-10-02 17:07:44 +0000

    modify setup further to match echopype

--- echoregions/__init__.py
+++ echoregions/__init__.py
@@ -7,5 +7,4 @@ that contain the majority of the functionality within echoregions.
 from .core import read_evl, read_evr
 from .utils.api import convert_mask_2d_to_3d, convert_mask_3d_to_2d
 
-__all__ = ["read_evl", "read_evr", "convert_mask_2d_to_3d", "convert_mask_3d_to_2d"]
-__version__ = "0.2.0"
+__all__ = ["read_evl", "read_evr", "convert_mask_2d_to_3d", "convert_mask_3d_to_2d"]  # noqa
--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
-from setuptools import find_packages, setup
+from __future__ import absolute_import, division, print_function
 
-# Dependencies
+from setuptools import setup
+
+# Dynamically read dependencies from requirements file
 with open("requirements.txt") as f:
     requirements = f.readlines()
-INSTALL_REQUIRES = [t.strip() for t in requirements]
 
-setup(
-    name="echoregions",
-    version="0.2.0",
-    author="Caesar Tuguinay",
-    author_email="ctuguina@uw.edu",
-    maintainer="Caesar Tuguinay",
-    maintainer_email="ctuguina@uw.edu",
-    description="Parsers and functions for working with EVR and EVL files",
-    url="https://github.com/OSOceanAcoustics/echoregions",
-    install_requires=INSTALL_REQUIRES,
-    packages=find_packages(),
-)
+if __name__ == "__main__":
+    setup(install_requires=requirements)
