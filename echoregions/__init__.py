abd2594d59427078745f0226451a8da6683bb9db
ctuguinay <cmtuguinay2000@gmail.com>
2023-10-02 16:48:29 +0000

    add version to init

--- echoregions/__init__.py
+++ echoregions/__init__.py
@@ -8,3 +8,4 @@ from .core import read_evl, read_evr
 from .utils.api import convert_mask_2d_to_3d, convert_mask_3d_to_2d
 
 __all__ = ["read_evl", "read_evr", "convert_mask_2d_to_3d", "convert_mask_3d_to_2d"]
+__version__ = "0.2.0"
