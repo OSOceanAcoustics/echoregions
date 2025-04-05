43dc3940656e4603f6fa92d3c297575980876ffc
Caesar Tuguinay <87830138+ctuguinay@users.noreply.github.com>
2025-04-05 00:39:34 -0700

    manually set version (#240)

--- echoregions/__init__.py
+++ echoregions/__init__.py
@@ -12,6 +12,8 @@ from .core import read_evl, read_evr, read_lines_csv, read_regions_csv
 from .regions2d.evr_writer import write_evr
 from .utils.api import convert_mask_2d_to_3d, convert_mask_3d_to_2d
 
+__version__ = "0.2.3"
+
 __all__ = [
     "read_evl",
     "read_lines_csv",
